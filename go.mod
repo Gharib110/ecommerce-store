4510f8ac64a9c4115ce6ec4cf87a3a6d2090ef27
AlirezaGharib <alirezagharib110@gmail.com>
2021-10-14 00:32:18 +0330

    card.go created

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/DapperBlondie/ecommerce-store
 
 go 1.17
 
-require github.com/go-chi/chi/v5 v5.0.4 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.0.4 // indirect
+	github.com/stripe/stripe-go/v72 v72.70.0 // indirect
+)
---
+++ internal/card/card.go
@@ -0,0 +1,15 @@
+package card
+
+type Card struct {
+	Secret   string
+	Key      string
+	Currency string
+}
+
+type Transaction struct {
+	TransactionStatusID int
+	Amount              int
+	Currency            string
+	LastFour            string
+	BankReturnCode      string
+}
