5f9b7861feeeb32e94e81a26092fb6c339ec76ac
AlirezaGharib <alirezagharib110@gmail.com>
2021-10-18 00:16:54 +0330

    Charge and CreatePaymentIntent is implemented

--- internal/card/card.go
+++ internal/card/card.go
@@ -1,15 +1,66 @@
 package card
 
+import (
+	"github.com/stripe/stripe-go/v72"
+	"github.com/stripe/stripe-go/v72/paymentintent"
+)
+
+// Card uses for holding the card details and data
 type Card struct {
 	Secret   string
 	Key      string
 	Currency string
 }
 
+// Transaction uses for holding and working with transaction data
 type Transaction struct {
 	TransactionStatusID int
 	Amount              int
 	Currency            string
 	LastFour            string
 	BankReturnCode      string
+}
+
+// Charge uses for charging the card
+func (c *Card) Charge(currency string, amount int) (*stripe.PaymentIntent, string, error) {
+	return c.CreatePaymentIntent(currency, amount)
+}
+
+// CreatePaymentIntent uses for creating payment intent
+func (c *Card) CreatePaymentIntent(currency string, amount int) (*stripe.PaymentIntent, string, error) {
+	stripe.Key = c.Secret
+
+	// create a payment intent
+	params := &stripe.PaymentIntentParams{
+		Amount:   stripe.Int64(int64(amount)),
+		Currency: stripe.String(currency),
+	}
+
+	//params.AddMetadata("Key", "Value")
+	pi, err := paymentintent.New(params)
+	if err != nil {
+		msg := ""
+		if stripeErr, ok := err.(*stripe.Error); ok {
+			msg = cardErrorMessage(stripeErr.Code)
+		}
+		return nil, msg, err
+	}
+
+	return pi, "", nil
+}
+
+// cardErrorMessage uses for creating error message based on stripe.ErrorCode
+func cardErrorMessage(code stripe.ErrorCode) string {
+	msg := "" //nolint:wastedassign
+
+	switch code { //nolint:wsl
+	case stripe.ErrorCodeCardDeclined:
+		msg = "Your card was declined"
+	case stripe.ErrorCodeExpiredCard:
+		msg = "Your card is expired"
+	default:
+		msg = "Some error occurred on your card"
+	}
+
+	return msg
 }
