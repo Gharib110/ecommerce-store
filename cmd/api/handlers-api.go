075ea3a3d76800bb3b6f86492c0fd2d1eefdf8c7
AlirezaGharib <alirezagharib110@gmail.com>
2021-10-31 00:42:14 +0330

    cards created, GetPaymentIntent was implemented completely

--- cmd/api/handlers-api.go
+++ cmd/api/handlers-api.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"github.com/DapperBlondie/ecommerce-store/internal/cards"
 	"net/http"
+	"strconv"
 )
 
 type stripePaylod struct {
@@ -12,26 +14,64 @@ type stripePaylod struct {
 
 type jsonResponse struct {
 	OK      bool   `json:"ok"`
-	Message string `json:"message"`
-	Content string `json:"content"`
-	ID      string `json:"id"`
+	Message string `json:"message,omitempty"`
+	Content string `json:"content,omitempty"`
+	ID      string `json:"id,omitempty"`
 }
 
 func (app *application) GetPaymentIntent(w http.ResponseWriter, r *http.Request) {
-	rsp := jsonResponse{
-		OK:      true,
-		Message: "",
-		Content: "",
-		ID:      "",
-	}
-	out, err := json.MarshalIndent(rsp, "", "	")
+	var payload stripePaylod
+
+	err := json.NewDecoder(r.Body).Decode(&payload)
 	if err != nil {
 		app.errLogger.Println(err)
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(out)
+	amount, err := strconv.Atoi(payload.Amount)
+	if err != nil {
+		app.errLogger.Println(err)
+		return
+	}
 
-	return
+	card := &cards.Card{
+		Secret:   app.config.stripe.secret,
+		Key:      app.config.stripe.key,
+		Currency: payload.Currency,
+	}
+
+	okay := true
+	pi, msg, err := card.Charge(payload.Currency, amount)
+	if err != nil {
+		okay = false
+	}
+
+	if okay {
+		out, err := json.MarshalIndent(pi, "", "		")
+		if err != nil {
+			app.errLogger.Println(err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(out)
+		return
+	} else {
+		rsp := jsonResponse{
+			OK:      true,
+			Message: msg,
+			Content: "",
+			ID:      "",
+		}
+		out, err := json.MarshalIndent(rsp, "", "	")
+		if err != nil {
+			app.errLogger.Println(err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(out)
+
+		return
+	}
 }
